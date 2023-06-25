ef1c1a61b5c801f1eca555663bbcd6fdb8747552
Jamal R <jamal.rabujev@gmail.com>
2023-06-25 17:49:31 +0200

    created dynamic html tables

--- frontend/src/app/core/denom-values.ts
+++ frontend/src/app/core/denom-values.ts
@@ -1,5 +1,10 @@
-//Class holds array of possible currency denominations.
+import { Injectable } from "@angular/core";
 
+@Injectable({
+    providedIn: 'root'
+  })
+  
+//Class holds array of possible currency denominations.
 export class DenomValues {
     public denomArr: number[] = 
         [200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01];
--- frontend/src/app/core/result-table/result-table.component.ts
+++ frontend/src/app/core/result-table/result-table.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DenomValues } from '../denom-values';
+
 
 @Component({
   selector: 'app-result-table',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./result-table.component.css']
 })
 export class ResultTableComponent {
+
+  //Injecting the object for possible denominations
+  // and using parameter Properties to simultaneously define it as a property
+  constructor(private denomValues: DenomValues) {};
+
+  //Initial Map with denoms as keys and 0 as values for now
+  private initialDenoms: Map<number, number> = this.denomValues.denomMap();
+  
+  result: Map<number, number> = new Map(this.denomValues.denomMap());
+
+  difference: Map<number, string> = new Map();
+
+  private useBackend: boolean = false;
+
+
+  ngOnInit(): void {
+  }
+  
+  //Fills result Map with the amounts of notes and coins 
+  calcResult(total: number) {
+
+    if (this.useBackend) {
+      
+     }
+    else {
+
+      let previousResult = new Map(this.result);
+      
+      let rest: number = total;
+      //for each currency denomination, calculates how many of them fit in the rest amount and fills result map
+      for (let key of this.result.keys()) {
+        let value: number = Math.floor(rest/key);
+        this.result.set(key, value)
+
+        rest = rest%key;
+      }
+      //calculating the difference
+      if (previousResult.size > 0) {
+
+      }
+
+      
+    }
+
+
+    
+
+  }
+
 
 }
