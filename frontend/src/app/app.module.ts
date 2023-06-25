11048cad71417eedd251db779feaea3f217be58e
Jamal R <jamal.rabujev@gmail.com>
2023-06-25 14:43:22 +0200

    front: created table components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResultTableComponent } from './core/result-table/result-table.component';
+import { DifferenceTableComponent } from './core/difference-table/difference-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultTableComponent,
+    DifferenceTableComponent
   ],
   imports: [
     BrowserModule
